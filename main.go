707677c72b6d4406483f60e7605f18c1324ca7ca
Tyler Hawkins <3319104+tyzbit@users.noreply.github.com>
2022-05-03 19:29:21 -0400

    fix: Guild intent

--- main.go
+++ main.go
@@ -143,7 +143,7 @@ func main() {
 	// We have to be explicit about what we want to receive. In addition,
 	// some intents require additional permissions, which must be granted
 	// to the bot when it's added or after the fact by a guild admin.
-	discordIntents := discordgo.IntentsGuildMessages |
+	discordIntents := discordgo.IntentsGuildMessages | discordgo.IntentsGuilds |
 		discordgo.IntentsDirectMessages | discordgo.IntentsDirectMessageReactions |
 		discordgo.IntentsGuildMessageReactions
 	dg.Identify.Intents = discordIntents

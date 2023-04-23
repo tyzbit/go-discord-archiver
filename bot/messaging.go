cd53ba0a090b14d6fa9994a0c7e53384db4e1457
Tyler Hawkins <3319104+tyzbit@users.noreply.github.com>
2023-04-23 17:58:46 -0400

    feat: (messaging) remove retry button after a period of time

--- bot/messaging.go
+++ bot/messaging.go
@@ -1,9 +1,14 @@
 package bot
 
 import (
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 	log "github.com/sirupsen/logrus"
 )
+
+// How long the bot waits before removing the retry button
+const removeRetryButtonWait = 30 * time.Second
 
 // sendArchiveResponse sends the message with a result from archive.org
 func (bot *ArchiverBot) sendArchiveResponse(message *discordgo.Message, reply *discordgo.MessageSend) error {
@@ -32,14 +37,39 @@ func (bot *ArchiverBot) sendArchiveResponse(message *discordgo.Message, reply *d
 		log.Debug("sending archive message response in ",
 			guild.Name, "(", guild.ID, "), calling user: ",
 			username, "(", message.Member.User.ID, ")")
+
+		message, err = bot.DG.ChannelMessageSendComplex(message.ChannelID, reply)
+		message.GuildID = guild.ID
+		go bot.removeRetryButtonAfterSleep(message)
 	} else {
 		log.Debug("declining archive message response in ",
 			"calling user: ", username, "(", message.Member.User.ID, ")")
 	}
 
-	_, err = bot.DG.ChannelMessageSendComplex(message.ChannelID, reply)
 	if err != nil {
 		log.Errorf("problem sending message: %v", err)
 	}
 	return nil
+}
+
+func (bot *ArchiverBot) removeRetryButtonAfterSleep(message *discordgo.Message) {
+	time.Sleep(removeRetryButtonWait)
+	me := discordgo.MessageEdit{
+		// Remove the components (button)
+		Components: []discordgo.MessageComponent{},
+		Embeds:     message.Embeds,
+		ID:         message.ID,
+		Channel:    message.ChannelID,
+	}
+
+	guild, gErr := bot.DG.Guild(message.GuildID)
+	if gErr != nil {
+		log.Errorf("unable to look up server by id: %v", message.GuildID)
+
+	}
+	_, err := bot.DG.ChannelMessageEditComplex(&me)
+	if err != nil {
+		log.Errorf("unable to remove retry button on message id %v, server: %s(%s): %v, ",
+			message.ID, message.GuildID, guild.Name, err)
+	}
 }
